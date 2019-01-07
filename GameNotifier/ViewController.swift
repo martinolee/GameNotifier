ca8a8563b7e8d75335dde38cff400767b2fa7ba9
Soo-Han Lee <martino_lee@naver.com>
2019-01-07 16:50:53 +0900

    Initial Commit

---
+++ GameNotifier/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GameNotifier
+//
+//  Created by 이수한 on 07/01/2019.
+//  Copyright © 2019 Lee Soo-Han. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
