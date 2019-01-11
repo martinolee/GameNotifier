b245b9d2c4e02955c00c4bc9d7b25b47d1593978
Soo-Han Lee <martino_lee@naver.com>
2019-01-11 22:04:58 +0900

    Add PL teams

--- GameNotifier/TeamsViewController.swift
+++ GameNotifier/TeamsViewController.swift
@@ -10,8 +10,19 @@ import UIKit
 
 class TeamsViewController: UITableViewController {
     
-    let teams: Array<String> = []
-
+    let teams: Dictionary<String, String> = [
+        "PL": "Liverpool"               , "PL": "Manchester City",
+        "PL": "Tottenham Hotspur"       , "PL": "Chelsea",
+        "PL": "Arsenal"                 , "PL": "Manchester United",
+        "PL": "Leicester City"          , "PL": "Watford",
+        "PL": "Wolverhampton Wanderers" , "PL": "West Ham United",
+        "PL": "Everton"                 , "PL": "Bournemouth",
+        "PL": "Brighton and Hove Albion", "PL": "Crystal Palace",
+        "PL": "Newcastle United"        , "PL": "Burnley",
+        "PL": "Cardiff City"            , "PL": "Southampton",
+        "PL": "Fulham"                  , "PL": "Huddersfield Town"
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
